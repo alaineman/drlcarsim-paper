5f0c09081b624297ac34b8f375135b98672e4a5e
Unox-Official <tobiasd@xs4all.nl>
2023-05-10 14:25:44 +0200

    Added discretized version of racetrack reward function

--- highway_env/envs/racetrack_env.py
+++ highway_env/envs/racetrack_env.py
@@ -356,6 +356,130 @@ class RacetrackEnvModified(RacetrackEnv):
             else:
                 self.road.vehicles.append(vehicle)
 
+class RacetrackEnvModifiedDiscrete(RacetrackEnv):
+    @classmethod
+    def default_config(cls) -> dict:
+        config = super().default_config()
+        config.update({
+            "simulation_frequency": 15,
+            "policy_frequency": 15,
+        })
+        return config
+
+    def _reward(self, action: np.ndarray) -> float:
+        reward = 0
+        rewards = self._rewards(action)
+        reward_weights = {'safety': 0.50, 'comfort': 0.25, 'efficiency': 0.25}
+        for key in rewards.keys():
+            reward += reward_weights[key] * rewards[key]
+        # apply the penalties for not abiding by the rules
+        # if self.vehicle.speed > speed_limit:
+        #     reward -= (self.vehicle.speed - speed_limit) / (self.vehicle.MAX_SPEED - speed_limit)
+
+        if not self.vehicle.on_road:
+            reward -= 1
+        if self.vehicle.crashed:
+            reward -= 100
+        #print('time for reward')
+        return reward
+
+        
+    def _rewards(self, action: np.ndarray) -> Dict[Text, float]:
+        #(2 * self.vehicle.speed) / (
+        #        self.vehicle.MAX_SPEED - self.vehicle.MIN_SPEED)  # max speed returns a reward of 1
+        TTC = None
+        obs_matrix = KinematicObservation(self, absolute=False, vehicles_count=self.config["other_vehicles"], normalize=False).observe()
+        use_TTC = False
+        glob_TTC = float('inf')
+        for vehicle in range(1,len(obs_matrix)):
+            x_pos = obs_matrix[vehicle][1]
+            y_pos = -1 * obs_matrix[vehicle][2]
+            pos_vec = [x_pos, y_pos] # this is relative when absolute = False
+            vx = obs_matrix[vehicle][3]
+            vy = -1 * obs_matrix[vehicle][4]
+            vel_vec = [vx, vy]
+            if np.dot(pos_vec, pos_vec) != 0:
+                proj_pos_vel = np.multiply(np.dot(vel_vec, pos_vec) / np.dot(pos_vec, pos_vec), pos_vec)
+                len_pos = np.linalg.norm(pos_vec)
+                len_proj = np.linalg.norm(proj_pos_vel)
+
+                if proj_pos_vel[0] * vel_vec[0] > 0 and proj_pos_vel[1] * vel_vec[1] > 0: # collinear so TTC infinite
+                    TTC = float('Inf')
+                else: 
+                    TTC = len_pos / len_proj  
+            else:
+                TTC = float('Inf')          
+            if TTC > 0: # just to be safe
+                glob_TTC = min(glob_TTC, TTC)
+        #print(glob_TTC)    
+        if glob_TTC < 3: # only care about TTC if crash is close
+            use_TTC = True
+
+
+        _, lateral = self.vehicle.lane.local_coordinates(self.vehicle.position)
+        if not use_TTC:   
+        # all reward components are normalized (in range 0 to 1) and we take the weighted average of them in _reward
+            safety_reward = 1 / (1 + self.config["lane_centering_cost"] * lateral ** 2) 
+        else:
+            #(1 + self.config["lane_centering_cost"] * lateral ** 2)
+            safety_reward = 0.8 * (1 - 2 / TTC) + 0.2 * 1 / (1 + self.config["lane_centering_cost"] * lateral ** 2) 
+            #(1 + 4 * self.vehicle.lane.distance(self.vehicle.position) ** 2) 
+        speed_limit = 10
+        if self.vehicle.speed <= speed_limit:
+            efficiency_reward = self.vehicle.speed / speed_limit
+        else:
+            efficiency_reward = 1 - self.vehicle.speed / (self.vehicle.MAX_SPEED)
+
+
+        comfort_reward = 1 - self.vehicle.jerk
+        rewards_keys = ['safety', 'comfort', 'efficiency']
+        rewards_values = [safety_reward, comfort_reward, efficiency_reward]
+        return dict(zip(rewards_keys, rewards_values))
+
+
+
+    def _make_vehicles(self) -> None:
+        """
+        Populate a road with several vehicles on the highway and on the merging lane, as well as an ego-vehicle.
+        """
+        rng = self.np_random
+
+        # Controlled vehicles
+        self.controlled_vehicles = []
+        for i in range(self.config["controlled_vehicles"]):
+            lane_index = ("a", "b", rng.integers(2)) if i == 0 else \
+                self.road.network.random_lane_index(rng)
+            controlled_vehicle = self.action_type.vehicle_class.make_on_lane(self.road, lane_index, speed=None,
+                                                                             longitudinal=rng.uniform(20, 50))
+
+            self.controlled_vehicles.append(controlled_vehicle)
+            self.road.vehicles.append(controlled_vehicle)
+
+        # Front vehicle
+        vehicle = IDMVehicle.make_on_lane(self.road, ("b", "c", lane_index[-1]),
+                                          longitudinal=rng.uniform(
+                                              low=0,
+                                              high=self.road.network.get_lane(("b", "c", 0)).length
+                                          ),
+                                          speed=6 + rng.uniform(high=3))
+        self.road.vehicles.append(vehicle)
+
+        # Other vehicles
+        for i in range(rng.integers(self.config["other_vehicles"])):
+            random_lane_index = self.road.network.random_lane_index(rng)
+            vehicle = IDMVehicle.make_on_lane(self.road, random_lane_index,
+                                              longitudinal=rng.uniform(
+                                                  low=0,
+                                                  high=self.road.network.get_lane(random_lane_index).length
+                                              ),
+                                              speed=6 + rng.uniform(high=3))
+            # Prevent early collisions
+            for v in self.road.vehicles:
+                if np.linalg.norm(vehicle.position - v.position) < 20:
+                    break
+            else:
+                self.road.vehicles.append(vehicle)
+
 
 class RacetrackEnvPrevBest(RacetrackEnv):
     @classmethod
@@ -452,7 +576,7 @@ class DiscreteRacetrackEnv(RacetrackEnv):
             "observation": {
                 "type": "Kinematics",
                 "vehicles_count": 5,
-                "features": ["presence", "x", "y", "vx", "vy", "lane_distance", "lane_heading_difference"],
+                #"features": ["presence", "x", "y", "vx", "vy", "lane_distance", "lane_heading_difference"],
             },
             "action": {
                 "type": "DiscreteAction",
@@ -464,14 +588,14 @@ class DiscreteRacetrackEnv(RacetrackEnv):
         })
         return config
 
-    def _reward(self, action: int) -> float:
-        reward = 0
-        ego_car = self.vehicle
-        reward -= ego_car.lane_distance**1.5
-        if ego_car.lane_heading_difference < 1.5:
-            reward += ego_car.speed
-        # reward -= ego_car.lane_heading_difference
-        return reward/25
+    #def _reward(self, action: int) -> float:
+    #    reward = 0
+    #    ego_car = self.vehicle
+    #    reward -= ego_car.lane_distance**1.5
+    #    if ego_car.lane_heading_difference < 1.5:
+    #        reward += ego_car.speed
+    #    # reward -= ego_car.lane_heading_difference
+    #    return reward/25
 
 class RacetrackEnvTest(RacetrackEnv):
     @classmethod
@@ -494,6 +618,11 @@ register(
 register(
     id='racetrack-v2',
     entry_point='highway_env.envs:RacetrackEnvModified'
+)
+
+register(
+    id='racetrack-v5',
+    entry_point='highway_env.envs:RacetrackEnvModifiedDiscrete'
 )
 
 register(
